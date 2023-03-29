7edf62b3850f004955417e72d82967322ab77fc8
Andrzej Pomirski <andrzej.pomirski@gmail.com>
2023-03-29 20:19:55 +0200

    Add most basic configuration for server
    This commit adds a basic pom.xml with a default configuration
    for a server that will be later expanded upon.

---
+++ src/main/java/com/pomirski/Server.java
@@ -0,0 +1,14 @@
+package com.pomirski;
+
+import org.takes.facets.fork.FkRegex;
+import org.takes.facets.fork.TkFork;
+import org.takes.http.Exit;
+import org.takes.http.FtBasic;
+
+public class Server {
+    public static void main(final String... args) throws Exception {
+        new FtBasic(
+                new TkFork(new FkRegex("/", "hello, world!")), 8080
+        ).start(Exit.NEVER);
+    }
+}
